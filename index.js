775db54ce587cf85da2d3089c7e7b1dcb79a3cba
powershell1 <175_40628@st.bcc1852.com>
2022-12-04 14:26:17 +0700

    merge fix

--- index.js
+++ index.js
@@ -1,248 +1,129 @@
-// Prototype function \\
-Element.prototype.insertChildAtIndex = function(child, index) {
-    if (!index) index = 0;
-    if (index === true) {
-        this.appendChild(child);
-    } else if (index >= this.children.length) {
-        this.appendChild(child);
-    } else {
-        this.insertBefore(child, this.children[index]);
-    };
+const express = require("express");
+const expressws = require("express-ws");
+const fileupload = require("express-fileupload");
+const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid");
+
+const app = express();
+expressws(app);
+app.use(fileupload());
+
+const every_ws = {};
+
+function IsJSON(json) {
+    try {
+        var a = JSON.parse(json);
+        return a;
+    } catch (e) {
+        return false;
+    }
 };
 
-
-
-
-// Main Script \\
-var LoadingText = document.getElementById("LoadingText");
-
-var host = document.location.host;
-var protocal = document.location.protocal;
-var towsp = protocal == "http:" ? "ws://" : "wss://";
-var websocket = new WebSocket(`${towsp}${host}/ws`);
-var loadingdot = 2;
-
-var name_inp = "default";
-var ping_interval;
-
-LoadingText.innerText = "Loading.";
-var LoadingInterval = setInterval(() => {
-    var Contac = "";
-    for (var i = 0; i < loadingdot; i++) { Contac += "." };
-    LoadingText.innerText = "Loading" + Contac;
-    loadingdot++;
-    if (loadingdot >= 4) {
-        loadingdot = 1;
-    };
-}, 1000);
-
-function GetDownloadName(link) {
-    var link_split = link.split("/get_upload/");
-    var name = link_split[link_split.length - 1];
-    return name;
-};
-
-function FileToLongName(file_name) {
-    var length = 15;
-    if (file_name.length > length) {
-        var long_name = file_name.split(".");
-        var long_name = long_name[0].slice(0, Math.floor(length / 3)) + "..." + long_name[0].slice(Math.floor(length / 1.25), file_name.length) + "." + long_name[1];
-        return long_name;
-    } else {
-        return file_name;
-    };
-};
-
-function AddText(pos, sender, body) {
-    return new Promise((resolve, reject) => {
-        var chat = document.getElementById("chat_msgshow");
-        var chat_div = document.createElement("div");
-        var chat_a = document.createElement("a");
-        chat_a.innerText = `${sender}: ${body}`;
-        chat_a.className = "chat_text";
-        chat_div.appendChild(chat_a);
-        chat.insertChildAtIndex(chat_div, pos);
-    });
-};
-
-function AddFile(pos, sender, url, mineType, filesize) {
-    return new Promise((resolve, reject) => {
-        var filetype = mineTypeCheck(mineType);
-        if (filetype == "image") {
-            var chat = document.getElementById("chat_msgshow");
-            var chat_div = document.createElement("div");
-            var chat_a = document.createElement("a");
-            var chat_img = document.createElement("img");
-            chat_a.innerText = `${sender}: `;
-            chat_a.className = "chat_text";
-            chat_img.src = url;
-            chat_img.className = "chat_img";
-            chat_div.appendChild(chat_a);
-            chat_div.appendChild(chat_img);
-            chat.insertChildAtIndex(chat_div, pos);
-            resolve();
-        } else if (filetype == "audio") {
-            var chat = document.getElementById("chat_msgshow");
-            var chat_div = document.createElement("div");
-            var chat_a = document.createElement("a");
-            var chat_audio = document.createElement("audio");
-            var chat_source = document.createElement("source");
-            chat_a.innerText = `${sender}: `;
-            chat_a.className = "chat_text";
-            chat_audio.controls = true;
-            chat_audio.autoplay = true;
-            chat_audio.className = "chat_audio";
-            chat_source.src = url;
-            chat_source.type = mineType;
-            chat_div.appendChild(chat_a);
-            chat_div.appendChild(chat_audio);
-            chat_audio.appendChild(chat_source);
-            chat.insertChildAtIndex(chat_div, pos);
-            resolve();
-        } else if (filetype == "video") {
-            var chat = document.getElementById("chat_msgshow");
-            var chat_div = document.createElement("div");
-            var chat_a = document.createElement("a");
-            var chat_video = document.createElement("video");
-            var chat_source = document.createElement("source");
-            chat_a.innerText = `${sender}: `;
-            chat_a.className = "chat_text";
-            chat_video.controls = true;
-            chat_video.autoplay = true;
-            chat_video.className = "chat_video";
-            chat_source.src = url;
-            chat_source.type = mineType;
-            chat_div.appendChild(chat_a);
-            chat_div.appendChild(chat_video);
-            chat_video.appendChild(chat_source);
-            chat.insertChildAtIndex(chat_div, pos);
-            resolve();
-        } else {
-            var download_link = GetDownloadName(url)
-            var chat = document.getElementById("chat_msgshow");
-            var chat_div = document.createElement("div");
-            var chat_a = document.createElement("a");
-            chat_a.innerText = `${sender}: `;
-            chat_a.className = "chat_text";
-            var chat_div2 = document.createElement("div");
-            chat_div2.className = "chat_download";
-            var chat_a2 = document.createElement("a");
-            chat_a2.className = "name";
-            chat_a2.href = `/download/${download_link}`;
-            chat_a2.innerText = FileToLongName(download_link);
-            var chat_a3 = document.createElement("a");
-            chat_a3.className = "size";
-            chat_a3.innerText = filesize.toFixed(2) + "MB";
-            var chat_img = document.createElement("img");
-            chat_img.src = "/public/download.png";
-            chat_img.className = "file_download";
-            chat_img.addEventListener("click", () => {
-                window.open(`/download/${download_link}`, "_blank");
-            });
-            chat_div.appendChild(chat_a);
-            chat_div.appendChild(chat_div2);
-            chat_div2.appendChild(chat_a2);
-            chat_div2.appendChild(chat_a3);
-            chat_div2.appendChild(chat_img);
-            chat.insertChildAtIndex(chat_div, pos);
-            resolve();
-        };
-    });
-};
-
-function mineTypeCheck(file) {
-    var file_type = file.split("/");
-    return file_type[0];
-};
-
-function renameFile(originalFile, newName) {
-    return new File([originalFile], newName, {
-        type: originalFile.type,
-        lastModified: originalFile.lastModified,
-    });
-}
-
-websocket.addEventListener("close", () => {
-    alert("[Warning] Connection to server lost!");
-    clearInterval(ping_interval);
-});
-
-websocket.addEventListener("message", async (event) => {
-    var data = JSON.parse(event.data);
-    if (data.type == "message") {
-        await AddText(true, data.name, data.text);
-    } else if (data.type == "file") {
-        await AddFile(true, data.name, data.url, data.file_type, (data.file_size / 1024));
-    } else if (data.type == "chat_history") {
-        var chatHistory = JSON.parse(data.body);
-        clearInterval(LoadingInterval);
-        document.getElementById("LoadingDIV").remove();
-        document.getElementById("enter_name").style = "";
-        chatHistory.history.forEach(async (element2, idx2) => {
-            if (element2.text) {
-                await AddText(idx2, element2.name, element2.text);
-            } else if (element2.url) {
-                var fetch_content = await fetch(element2.url)
-                await AddFile(idx2, element2.name, element2.url, fetch_content.headers.get("Content-Type"), (parseInt(fetch_content.headers.get("Content-Length"), 10) / 1024));
+app.ws("/ws", (ws, req) => {
+    const id = uuid.v4();
+    every_ws[id] = ws;
+    ws.on("message", msg => {
+        var isJson = IsJSON(msg);
+        if (isJson) {
+            var data = JSON.parse(msg);
+            if (data.type == "message") {
+                if (data.name == "") {
+                    data.name = "Anonymous";
+                };
+                var data_read = JSON.parse(fs.readFileSync(path.join(__dirname, "chat.json")));
+                data_read["history"].push({
+                    "name": data.name,
+                    "text": data.body
+                });
+                fs.writeFileSync(path.join(__dirname, "chat.json"), JSON.stringify(data_read));
+                for (const property in every_ws) {
+                    var socket = every_ws[property];
+                    socket.send(JSON.stringify({
+                        "type": "message",
+                        "name": data.name,
+                        "text": data.body
+                    }));
+                };
+            } else if (data.type == "chat_history") {
+                var chatHistory = fs.readFileSync("chat.json", "utf8");
+                ws.send(JSON.stringify({
+                    type: "chat_history",
+                    body: chatHistory
+                }));
             };
-        });
-    };
-});
-
-document.getElementById("name_btn").addEventListener("click", () => {
-    var yes_prompt = false;
-    if (document.getElementById("name_input").value.length > 0) {
-        yes_prompt = true;
-    } else {
-        yes_prompt = confirm("Are you sure you want to be anonymous?");
-    };
-    if (yes_prompt) {
-        name_inp = document.getElementById("name_input").value;
-        document.getElementById("enter_name").remove();
-        document.getElementById("chat").style = "";
-    };
-});
-
-document.getElementById("file_upload").addEventListener("click", () => {
-    var new_file = document.createElement("input");
-    new_file.type = "file";
-    new_file.onchange = () => {
-        var files = Array.from(new_file.files);
-        new_file.remove();
-        files.forEach(file => {
-            var formData = new FormData();
-            var newfile_name = btoa(encodeURIComponent(file.name));
-            formData.append("files", renameFile(file, newfile_name));
-            fetch("/upload", {
-                method: "POST",
-                headers: {
-                    "name": name_inp,
-                    "file_name": newfile_name
-                },
-                body: formData
-            });
-        });
-    };
-    new_file.click();
-});
-
-document.addEventListener("keypress", (key) => {
-    if (key.key == "Enter" && !key.shiftKey) {
-        var chat_msg = document.getElementById("chat_msg");
-        if (chat_msg.value.length > 0) {
-            chat_msg.blur();
-            websocket.send(JSON.stringify({
-                "type": "message",
-                "name": name_inp,
-                "body": chat_msg.value
-            }));
-            chat_msg.value = "";
         };
+    });
+    ws.on("close", () => {
+        delete every_ws[id];
+    });
+    var chatHistory = fs.readFileSync("chat.json", "utf8");
+    ws.send(JSON.stringify({
+        type: "chat_history",
+        body: chatHistory
+    }));
+});
+
+app.post("/upload", (req, res) => {
+    var file_selected = req.files.files;
+    if (!file_selected) { res.send("File not found!"); return; };
+    var file_name = decodeURIComponent(atob(req.headers.file_name));
+    fs.writeFileSync(path.join(__dirname, "uploads", file_name), file_selected.data);
+    if (req.headers.name == "") {
+        req.headers.name = "Anonymous";
+    };
+    var data_read = JSON.parse(fs.readFileSync(path.join(__dirname, "chat.json")));
+    data_read["history"].push({
+        "name": req.headers.name,
+        "url": "/get_upload/" + file_name,
+    });
+    fs.writeFileSync(path.join(__dirname, "chat.json"), JSON.stringify(data_read));
+    for (const property in every_ws) {
+        var socket = every_ws[property];
+        socket.send(JSON.stringify({
+            "type": "file",
+            "name": req.headers.name,
+            "url": "/get_upload/" + file_name,
+            "file_type": file_selected.mimetype,
+            "file_size": file_selected.size
+        }));
+    };
+    res.send("Uploaded!");
+});
+
+app.use("/download", (req, res, next) => {
+    var decode = decodeURIComponent(req.url);
+    var find_src = path.join(__dirname, "uploads", decode);
+    if (fs.existsSync(find_src)) {
+        res.download(find_src);
+    } else {
+        next();
     };
 });
 
-ping_interval = setInterval(() => {
-    websocket.send(JSON.stringify({
-        type: "ping"
-    }));
-}, 1000);
+app.use("/get_upload", (req, res, next) => {
+    var decode = decodeURIComponent(req.url);
+    var find_src = path.join(__dirname, "uploads", decode);
+    if (fs.existsSync(find_src)) {
+        res.sendFile(find_src);
+    } else {
+        next();
+    };
+});
+
+app.use("/public", (req, res, next) => {
+    var find_src = path.join(__dirname, "public", req.path);
+    if (fs.existsSync(find_src)) {
+        res.sendFile(find_src);
+    } else {
+        next();
+    };
+});
+
+app.use((req, res, next) => {
+    res.sendFile(__dirname + "/src/chat.html");
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000!");
+});
--- public/script.js
+++ public/script.js
@@ -1,8 +1,24 @@
+// Prototype function \\
+Element.prototype.insertChildAtIndex = function(child, index) {
+    if (!index) index = 0;
+    if (index === true) {
+        this.appendChild(child);
+    } else if (index >= this.children.length) {
+        this.appendChild(child);
+    } else {
+        this.insertBefore(child, this.children[index]);
+    };
+};
+
+
+
+
+// Main Script \\
 var LoadingText = document.getElementById("LoadingText");
 
 var host = document.location.host;
 var protocal = document.location.protocal;
-var towsp = protocal == "https:" ? "wss://" : "ws://";
+var towsp = protocal == "http:" ? "ws://" : "wss://";
 var websocket = new WebSocket(`${towsp}${host}/ws`);
 var loadingdot = 2;
 
@@ -19,16 +35,6 @@ var LoadingInterval = setInterval(() => {
         loadingdot = 1;
     };
 }, 1000);
-
-function AddText(sender, body) {
-    var chat = document.getElementById("chat_msgshow");
-    var chat_div = document.createElement("div");
-    var chat_a = document.createElement("a");
-    chat_a.innerText = `${sender}: ${body}`;
-    chat_a.className = "chat_text";
-    chat_div.appendChild(chat_a);
-    chat.appendChild(chat_div);
-};
 
 function GetDownloadName(link) {
     var link_split = link.split("/get_upload/");
@@ -47,83 +53,105 @@ function FileToLongName(file_name) {
     };
 };
 
-function AddFile(sender, url, mineType, filesize) {
-    var filetype = mineTypeCheck(mineType);
-    if (filetype == "image") {
+function AddText(pos, sender, body) {
+    return new Promise((resolve, reject) => {
         var chat = document.getElementById("chat_msgshow");
         var chat_div = document.createElement("div");
         var chat_a = document.createElement("a");
-        var chat_img = document.createElement("img");
-        chat_a.innerText = `${sender}: `;
-        chat_a.className = "chat_text";
-        chat_img.src = url;
-        chat_img.className = "chat_img";
-        chat_div.appendChild(chat_a);
-        chat_div.appendChild(chat_img);
-        chat.appendChild(chat_div);
-    } else if (filetype == "audio") {
-        var chat = document.getElementById("chat_msgshow");
-        var chat_div = document.createElement("div");
-        var chat_a = document.createElement("a");
-        var chat_audio = document.createElement("audio");
-        var chat_source = document.createElement("source");
-        chat_a.innerText = `${sender}: `;
-        chat_a.className = "chat_text";
-        chat_audio.controls = true;
-        chat_audio.autoplay = true;
-        chat_audio.className = "chat_audio";
-        chat_source.src = url;
-        chat_source.type = mineType;
-        chat_div.appendChild(chat_a);
-        chat_div.appendChild(chat_audio);
-        chat_audio.appendChild(chat_source);
-        chat.appendChild(chat_div);
-    } else if (filetype == "video") {
-        var chat = document.getElementById("chat_msgshow");
-        var chat_div = document.createElement("div");
-        var chat_a = document.createElement("a");
-        var chat_video = document.createElement("video");
-        var chat_source = document.createElement("source");
-        chat_a.innerText = `${sender}: `;
-        chat_a.className = "chat_text";
-        chat_video.controls = true;
-        chat_video.autoplay = true;
-        chat_video.className = "chat_video";
-        chat_source.src = url;
-        chat_source.type = mineType;
-        chat_div.appendChild(chat_a);
-        chat_div.appendChild(chat_video);
-        chat_video.appendChild(chat_source);
-        chat.appendChild(chat_div);
-    } else {
-        var download_link = GetDownloadName(url)
-        var chat = document.getElementById("chat_msgshow");
-        var chat_div = document.createElement("div");
-        var chat_a = document.createElement("a");
-        chat_a.innerText = `${sender}: `;
-        chat_a.className = "chat_text";
-        var chat_div2 = document.createElement("div");
-        chat_div2.className = "chat_download";
         var chat_a2 = document.createElement("a");
-        chat_a2.className = "name";
-        chat_a2.href = `/download/${download_link}`;
-        chat_a2.innerText = FileToLongName(download_link);
-        var chat_a3 = document.createElement("a");
-        chat_a3.className = "size";
-        chat_a3.innerText = filesize.toFixed(2) + "MB";
-        var chat_img = document.createElement("img");
-        chat_img.src = "/public/download.png";
-        chat_img.className = "file_download";
-        chat_img.addEventListener("click", () => {
-            window.open(`/download/${download_link}`, "_blank");
-        });
+        chat_a.innerText = `${sender}`;
+        chat_a.className = "chat_text";
+        chat_a2.innerText = `: ${body}`;
+        chat_a2.className = "chat_text";
         chat_div.appendChild(chat_a);
-        chat_div.appendChild(chat_div2);
-        chat_div2.appendChild(chat_a2);
-        chat_div2.appendChild(chat_a3);
-        chat_div2.appendChild(chat_img);
-        chat.appendChild(chat_div);
-    };
+        chat_div.appendChild(chat_a2);
+        chat.insertChildAtIndex(chat_div, pos);
+    });
+};
+
+function AddFile(pos, sender, url, mineType, filesize) {
+    return new Promise((resolve, reject) => {
+        var filetype = mineTypeCheck(mineType);
+        if (filetype == "image") {
+            var chat = document.getElementById("chat_msgshow");
+            var chat_div = document.createElement("div");
+            var chat_a = document.createElement("a");
+            var chat_img = document.createElement("img");
+            chat_a.innerText = `${sender}: `;
+            chat_a.className = "chat_text";
+            chat_img.src = url;
+            chat_img.className = "chat_img";
+            chat_div.appendChild(chat_a);
+            chat_div.appendChild(chat_img);
+            chat.insertChildAtIndex(chat_div, pos);
+            resolve();
+        } else if (filetype == "audio") {
+            var chat = document.getElementById("chat_msgshow");
+            var chat_div = document.createElement("div");
+            var chat_a = document.createElement("a");
+            var chat_audio = document.createElement("audio");
+            var chat_source = document.createElement("source");
+            chat_a.innerText = `${sender}: `;
+            chat_a.className = "chat_text";
+            chat_audio.controls = true;
+            chat_audio.autoplay = true;
+            chat_audio.className = "chat_audio";
+            chat_source.src = url;
+            chat_source.type = mineType;
+            chat_div.appendChild(chat_a);
+            chat_div.appendChild(chat_audio);
+            chat_audio.appendChild(chat_source);
+            chat.insertChildAtIndex(chat_div, pos);
+            resolve();
+        } else if (filetype == "video") {
+            var chat = document.getElementById("chat_msgshow");
+            var chat_div = document.createElement("div");
+            var chat_a = document.createElement("a");
+            var chat_video = document.createElement("video");
+            var chat_source = document.createElement("source");
+            chat_a.innerText = `${sender}: `;
+            chat_a.className = "chat_text";
+            chat_video.controls = true;
+            chat_video.autoplay = true;
+            chat_video.className = "chat_video";
+            chat_source.src = url;
+            chat_source.type = mineType;
+            chat_div.appendChild(chat_a);
+            chat_div.appendChild(chat_video);
+            chat_video.appendChild(chat_source);
+            chat.insertChildAtIndex(chat_div, pos);
+            resolve();
+        } else {
+            var download_link = GetDownloadName(url)
+            var chat = document.getElementById("chat_msgshow");
+            var chat_div = document.createElement("div");
+            var chat_a = document.createElement("a");
+            chat_a.innerText = `${sender}: `;
+            chat_a.className = "chat_text";
+            var chat_div2 = document.createElement("div");
+            chat_div2.className = "chat_download";
+            var chat_a2 = document.createElement("a");
+            chat_a2.className = "name";
+            chat_a2.href = `/download/${download_link}`;
+            chat_a2.innerText = FileToLongName(download_link);
+            var chat_a3 = document.createElement("a");
+            chat_a3.className = "size";
+            chat_a3.innerText = filesize.toFixed(2) + "MB";
+            var chat_img = document.createElement("img");
+            chat_img.src = "/public/download.png";
+            chat_img.className = "file_download";
+            chat_img.addEventListener("click", () => {
+                window.open(`/download/${download_link}`, "_blank");
+            });
+            chat_div.appendChild(chat_a);
+            chat_div.appendChild(chat_div2);
+            chat_div2.appendChild(chat_a2);
+            chat_div2.appendChild(chat_a3);
+            chat_div2.appendChild(chat_img);
+            chat.insertChildAtIndex(chat_div, pos);
+            resolve();
+        };
+    });
 };
 
 function mineTypeCheck(file) {
@@ -143,24 +171,23 @@ websocket.addEventListener("close", () => {
     clearInterval(ping_interval);
 });
 
-websocket.addEventListener("message", (event) => {
+websocket.addEventListener("message", async (event) => {
     var data = JSON.parse(event.data);
     if (data.type == "message") {
-        AddText(data.name, data.text);
+        await AddText(true, data.name, data.text);
     } else if (data.type == "file") {
-        AddFile(data.name, data.url, data.file_type, (data.file_size / 1024));
+        await AddFile(true, data.name, data.url, data.file_type, (data.file_size / 1024));
     } else if (data.type == "chat_history") {
         var chatHistory = JSON.parse(data.body);
         clearInterval(LoadingInterval);
         document.getElementById("LoadingDIV").remove();
         document.getElementById("enter_name").style = "";
-        chatHistory.history.forEach(element2 => {
+        chatHistory.history.forEach(async (element2, idx2) => {
             if (element2.text) {
-                AddText(element2.name, element2.text);
+                await AddText(idx2, element2.name, element2.text);
             } else if (element2.url) {
-                fetch(element2.url).then((res) => {
-                    AddFile(element2.name, element2.url, res.headers.get("Content-Type"), (parseInt(res.headers.get("Content-Length"), 10) / 1024));
-                });
+                var fetch_content = await fetch(element2.url)
+                await AddFile(idx2, element2.name, element2.url, fetch_content.headers.get("Content-Type"), (parseInt(fetch_content.headers.get("Content-Length"), 10) / 1024));
             };
         });
     };
